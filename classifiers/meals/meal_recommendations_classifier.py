a700a1a38ee1f7af6563c3df84fcb8df70acadd7
nativacu <nativacu@gmail.com>
2021-11-05 01:53:28 -0400

    WIP tuning models

--- classifiers/meals/meal_recommendations_classifier.py
+++ classifiers/meals/meal_recommendations_classifier.py
@@ -9,11 +9,19 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def get_recommendations(nutrients: RequiredMealNutrients):
-    meals = pd.read_csv(ROOT_DIR + './meals.csv')
+    meals = pd.read_csv(ROOT_DIR + '/meals.csv')
+
+    print("processing...")
+
+    for i, row in meals.iterrows():
+        meals.at[i, 'Protein/g'] = int(meals.at[i, 'Protein/g']) * int(meals.at[i, 'weightPerServing'])
+        meals.at[i, 'Fat/g'] = int(meals.at[i, 'Fat/g']) * int(meals.at[i, 'weightPerServing'])
+        meals.at[i, 'Carbohydrates/g'] = int(meals.at[i, 'Carbohydrates/g']) * int(meals.at[i, 'weightPerServing'])
 
     x = meals[['calories', 'Protein/g', 'Fat/g', 'Carbohydrates/g']]
-    meal_rows = meals[['id', 'title']]
-    nbrs = NearestNeighbors(n_neighbors=5, algorithm='auto').fit(x.sample(frac=0.50))
+    meal_rows = meals[['id', 'title', 'calories', 'Protein/g', 'Fat/g', 'Carbohydrates/g']]
+
+    nbrs = NearestNeighbors(n_neighbors=5, algorithm='auto', metric='euclidean').fit(x)
 
     required_nutrients = {'calories': [nutrients.calories / 3], 'Protein/g': [nutrients.proteins / 3],
                           'Fat/g': [nutrients.fats / 3], 'Carbohydrates/g': [nutrients.carbohydrates / 3]}
@@ -27,7 +35,11 @@ def get_recommendations(nutrients: RequiredMealNutrients):
     for i in indexes[0]:
         meal_names.append({
             'id': int(meal_rows.loc[i]['id']),
-            'name': meal_rows.loc[i]['title']
+            'name': meal_rows.loc[i]['title'],
+            'calories': float(meal_rows.loc[i]['calories']),
+            'Protein/g': float(meal_rows.loc[i]['Protein/g']),
+            'Fat/g': float(meal_rows.loc[i]['Fat/g']),
+            'Carbohydrates/g': float(meal_rows.loc[i]['Carbohydrates/g'])
         })
 
     return meal_names
